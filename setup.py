7769fd80829e126fbf09aabbaf9cb8f3e82ceb01
Abdelhadi DANBA <abdelhadi.danba@influans.com>
2018-11-24 00:17:08 +0100

    remove package in the setup file

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ setup(
     description='hello world example using module.',
     author='adanba',
     packages=find_packages(),
-    install_requires=[
-                  'numpy >= 1.14.3',
-                  'pyprg >= 0.1.1b7'
-                        ]
+    install_requires=[ ]
 )
 
